266ff57d9770564c2ed6e954a5889807f5f6e363
Mititelu Iosif Marian <mititelu.iosifmarian@yahoo.ro>
2017-04-11 17:05:56 +0300

    React integration

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+var config = {
+   entry: './main.js',
+
+   output: {
+      path: path.resolve(__dirname, './dist'),
+      filename: 'index.js',
+   },
+
+   devServer: {
+      inline: true,
+      port: 8080
+   },
+
+   module: {
+      loaders: [
+         {
+            test: /\.jsx?$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+
+            query: {
+               presets: ['es2015', 'react']
+            }
+         }
+      ]
+   }
+}
+
+module.exports = config;
